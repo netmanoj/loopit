9f7cfbf00a15cc014c9b47b3a3d160ed4717bc73
manoj-adhikari <imadhikarimanoj@gmail.com>
2025-03-09 15:16:30 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
 
 export default function Home() {
